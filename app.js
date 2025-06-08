1de08fc9e49d8cd56ea4ddcf03a7d54d3c869de3
Oleksandr Kulyk <oleku@smart-it.com>
2025-06-08 16:06:49 +0300

    Refactor database connection to use config file and add .gitignore

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const mongoose = require('mongoose');
 //other
 const booksRouter = require('./routes/api/books'); // Importing the books router
 const app = require('./server'); // Importing the server configuration
-
-const dbURI =
-  'mongodb+srv://alexander:n36KiOXW8LvwDqil@cluster0.x080npf.mongodb.net/books_reader?retryWrites=true&w=majority&appName=Cluster0';
+const { dbURI } = require('./config');
 
 mongoose.set('strictQuery', false); // Disable strict query mode
 
